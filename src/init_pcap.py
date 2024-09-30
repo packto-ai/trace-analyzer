b81c725a13c83a424fd5cc116621a35e792f82ec
Sartaj Singh <sartajs2002@gmail.com>
2024-09-30 14:52:59 -0400

    successfully compartmentalized graph configuration

--- src/init_pcap.py
+++ src/init_pcap.py
@@ -23,6 +23,7 @@ def init_pcap(true_PCAP_path):
     from tools.find_protocols import find_protocols
     from db_config import execute_query, create_connection, fetch_query
     from serialize import convert_to_json, deserialize_json
+    from config_graph import config_graph
 
     state_file = 'src/app_state.json'
     default_state = init_json()
@@ -35,22 +36,9 @@ def init_pcap(true_PCAP_path):
     #environment variables
     mistral_key = os.getenv('MISTRAL_API_KEY')
     llm = ChatMistralAI(model="mistral-large-latest", temperature=0)
-    
-    primary_assistant_prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You are a helpful customer support assistant for analyzing packet traces. "
-                " Use the provided tools to search for protocols, security threats, and other information to assist the user's queries. "
-                " When searching, be persistent. Expand your query bounds if the first search returns no results. "
-                " If a search comes up empty, expand your search before giving up."
-                " Use {PCAP} as the file to analyze."
-                " Use {external_context} as extra context."
-            ),
-            MessagesPlaceholder("messages"),
-        ]
-    )
 
+    base = os.path.splitext(PCAP_File.name)
+    base_pcap = base[0]
 
     connection = create_connection()
     this_pcap_id = 0
@@ -64,73 +52,8 @@ def init_pcap(true_PCAP_path):
 
         connection.close()
 
+    graph = config_graph()
 
-    tools = [find_protocols]
-
-    base = os.path.splitext(PCAP_File.name)
-    base_pcap = base[0]
-
-    llm_with_tools = llm.bind_tools(tools)
-
-    packto_runnable = primary_assistant_prompt | llm_with_tools
-
-    """
-    Define graph state
-    This makes it so that every message will be in a list
-    and all of those messages will be received to every node
-    in the graph when it is called. Nodes are basically just 
-    functions. So those functions will be called and they will 
-    receive messages state as input
-    """
-    class AgentState(TypedDict):
-        messages: Annotated[list, add_messages]#Annotated[Sequence[BaseMessage], operator.add]
-        PCAP: str
-        external_context: dict
-
-    #Node to see if we need to keep calling tools to answer the question
-    def should_continue(state):
-        return "continue" if state["messages"][-1].tool_calls else "end"
-
-    #call the model using the previous messages
-    def call_model(state, config):
-        response = packto_runnable.invoke(state)
-        return {"messages": [response]}
-
-    #execute the tool
-    def _invoke_tool(tool_call):
-        tool = {tool.name: tool for tool in tools}[tool_call["name"]]
-        return ToolMessage(tool.invoke(tool_call["args"]), tool_call_id=tool_call["id"])
-
-    tool_executor = RunnableLambda(_invoke_tool)
-
-    #Decide which tool is best
-    def call_tools(state):
-        last_message = state["messages"][-1]
-        return {"messages": tool_executor.batch(last_message.tool_calls)}
-
-    memory = MemorySaver()
-
-    workflow = StateGraph(AgentState)
-    workflow.add_node("agent", call_model)
-    workflow.add_node("action", call_tools)
-    workflow.set_entry_point("agent")
-
-    """
-    Conditional edge so that every time the agent is finished,
-    we check if it should continue. If we should, call more tools.
-    If not, end.
-    """
-    workflow.add_conditional_edges(
-        "agent",
-        should_continue,
-        {
-            "continue": "action",
-            "end": END,
-        },
-    )
-    #Allow a connection between action and agent so that the model can call tools
-    workflow.add_edge("action", "agent")
-    graph = workflow.compile(checkpointer=memory)
     input = {
         "messages": [HumanMessage("What protocols do you see in the trace?"),],
         "PCAP": true_PCAP_path,
@@ -146,16 +69,15 @@ def init_pcap(true_PCAP_path):
 
     json_app_state = convert_to_json(app_state)
 
-    connection = create_connection()
 
+    connection = create_connection()
     if connection:
         update_query = """
         UPDATE pcaps
-        SET graph_state = %s,
-            init_qa = %s
+        SET graph_state = %s
         WHERE pcap_id = %s;
         """
-        execute_query(connection, update_query, (json_app_state, json_app_state, this_pcap_id))
+        execute_query(connection, update_query, (json_app_state, this_pcap_id))
 
 init_pcap("uploads/TestPcap.pcapng")
 
