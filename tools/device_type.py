4303b15e268d94f01cd6e08edd7968623485a760
Sartaj Singh <sartajs2002@gmail.com>
2024-10-23 12:58:38 -0400

    Working with group of PCAPs

--- tools/device_type.py
+++ tools/device_type.py
@@ -3,33 +3,49 @@ from langchain_core.tools import tool
 import asyncio
 import sys
 import os
+from typing import List
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.db_config import fetch_query, execute_query, create_connection
 
 
-@tool
-def device_type(PCAP: str, MAC: str) -> str:
+#@tool
+def device_type(PCAPs: List[str], MAC: str) -> str:
     """
     Tool to find if a given device, denoted by the MAC argument is a client
     device, server device, or router.
     """
 
+    placeholder_list = ', '.join(['%s'] * len(PCAPs))
+    group_id = None
+
+    #JOINT TABLE TO QUERY ALL FILES IN A PARTICULAR SESSION
+    connection = create_connection()
+    if connection:
+        join_query = f"""
+        SELECT pcap_groups.group_id
+        FROM pcaps
+        JOIN pcap_groups ON pcap_groups.group_id = pcaps.group_id
+        WHERE pcaps.pcap_filepath IN ({placeholder_list});
+        """
+        output = fetch_query(connection, join_query, (tuple(PCAPs,)))
+        group_id = output[0][0]
+
     connection = create_connection()
     if connection:
         select_sql_query = """
         SELECT subnet
-        FROM pcaps
-        WHERE pcap_filepath = %s
+        FROM pcap_groups
+        WHERE group_id = %s
         """
-        output = fetch_query(connection, select_sql_query, (PCAP,))
+        output = fetch_query(connection, select_sql_query, (group_id,))
 
         connection.close()
 
     subnet = str(output[0][0])
     mask = str(subnet.rsplit('.', 1)[0])
 
-    # Load the pcapng file
-    capture = pyshark.FileCapture(PCAP)
+
+
 
     #arrays to store ips to see if the mac address corresponds to one or multiple
     #if just one src, it is a client. if one dest, it is prob a server. 
@@ -37,15 +53,19 @@ def device_type(PCAP: str, MAC: str) -> str:
     source_ips = []
     dest_ips = []
 
-    for packet in capture:
-        if ('eth' in packet and 'ip' in packet):
-            if str(packet.eth.src) == MAC:
-                if packet.ip.src not in source_ips:
-                    source_ips.append(packet.ip.src)
-            if str(packet.eth.dst) == MAC:
-                if packet.ip.dst not in dest_ips:
-                    dest_ips.append(packet.ip.dst)
-    capture.close()
+    # Load the pcapng file
+    for PCAP in PCAPs:
+        capture = pyshark.FileCapture(PCAP)
+
+        for packet in capture:
+            if ('eth' in packet and 'ip' in packet):
+                if str(packet.eth.src) == MAC:
+                    if packet.ip.src not in source_ips:
+                        source_ips.append(packet.ip.src)
+                if str(packet.eth.dst) == MAC:
+                    if packet.ip.dst not in dest_ips:
+                        dest_ips.append(packet.ip.dst)
+        capture.close()
 
     if (len(source_ips) > 1 and len(dest_ips) > 1):
         return "Router"
@@ -57,4 +77,4 @@ def device_type(PCAP: str, MAC: str) -> str:
     return "Unclear what type of device this is"
 
 # print(device_type("Trace.pcapng", "10:3d:1c:46:b9:46"))
-# print(device_type("Trace.pcapng", "4c:22:f3:bc:b7:18"))
+print(device_type(["Trace.pcapng", "Trace2.pcapng"], "4c:22:f3:bc:b7:18"))
