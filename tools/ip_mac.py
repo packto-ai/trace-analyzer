4c5a4a05bb3813539b83af204a171f16af69d570
Sartaj Singh <sartajs2002@gmail.com>
2024-10-24 17:29:34 -0400

    ip_mac tool working now

--- tools/ip_mac.py
+++ tools/ip_mac.py
@@ -4,26 +4,26 @@ import asyncio
 import pyshark.packet
 import pyshark.packet.packet
 import json
+import scapy
+from scapy.all import rdpcap, Ether, IP
+from typing import List
 
 @tool
-def ip_mac(PCAP: str) -> list:
+def ip_mac(PCAPs: List[str]) -> list:
     """
     Tool to find the all the MAC addresses and their corresponding IP addresses
     """
-
-    # Load the pcapng file
-    capture = pyshark.FileCapture(PCAP)
-
-    subnet = None
-
     mappings = []
+    for PCAP in PCAPs:
+        # Load the pcapng file
+        capture = rdpcap(PCAP)
 
-    for packet in capture:
-        mapping_dict = {}
-        if ('eth' in packet and 'ip' in packet):
-            mapping_dict.update({packet.eth.src: packet.ip.src})
-            if mapping_dict not in mappings:
-                mappings.append(mapping_dict)
+        for packet in capture:
+            mapping_dict = {}
+            if (packet.haslayer(Ether) and packet.haslayer(IP)):
+                mapping_dict.update({packet[Ether].src: packet[IP].src})
+                if mapping_dict not in mappings:
+                    mappings.append(mapping_dict)
 
     seen_keys = set()
     unique_mappings = []
@@ -34,11 +34,10 @@ def ip_mac(PCAP: str) -> list:
                 seen_keys.add(key)
                 unique_mappings.append(d)
 
-    capture.close()
 
     return json.dumps(unique_mappings)
 
-# print(ip_mac("Trace.pcapng"))
+# print(ip_mac(["Trace.pcapng", "Trace2.pcapng"]))
 
 """
 UNFINISHED
