b24dc9a6dd26e94e78effb15c49ed8d750c43432
Sartaj Singh <sartajs2002@gmail.com>
2024-10-30 12:55:33 -0400

    got rid of the keys.env reference in db_config and updated docker-compose to have the web container use database_url as env variable

--- src/db_config.py
+++ src/db_config.py
@@ -4,22 +4,14 @@ from dotenv import load_dotenv
 import os
 import pathlib
 
-BASE_DIR = pathlib.Path(__file__).parent.parent
-keys_path = os.path.join(BASE_DIR, 'keys.env')
-load_dotenv(dotenv_path=keys_path)
 
-db_host = os.getenv('DB_HOST')
-db_pass = os.getenv('DB_PASS')
-db_user = os.getenv('DB_USER')
+#load the db_url from docker-compose environment variables in the web container
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def create_connection():
     try:
         connection = psycopg2.connect(
-            database="packto_db",
-            user=db_user,
-            password=db_pass,
-            host=db_host,#"host.docker.internal",
-            port="5432"
+            DATABASE_URL
         )
         print("Connection to PostgreSQL DB successful")
         return connection
@@ -61,61 +53,61 @@ def fetch_query(connection, query, params=None):
         return []
 
 
-connection = create_connection()
-if connection:
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS pcap_groups (
-        group_id SERIAL PRIMARY KEY,
-        group_name TEXT NOT NULL UNIQUE,
-        group_path TEXT NOT NULL UNIQUE,
-        subnet TEXT,
-        chat_history JSONB,
-        init_qa JSONB,
-        graph_state JSONB
-    );  
-    '''
-    execute_query(connection, create_table_query)
+# connection = create_connection()
+# if connection:
+#     create_table_query = '''
+#     CREATE TABLE IF NOT EXISTS pcap_groups (
+#         group_id SERIAL PRIMARY KEY,
+#         group_name TEXT NOT NULL UNIQUE,
+#         group_path TEXT NOT NULL UNIQUE,
+#         subnet TEXT,
+#         chat_history JSONB,
+#         init_qa JSONB,
+#         graph_state JSONB
+#     );  
+#     '''
+#     execute_query(connection, create_table_query)
 
-    connection.close()
+#     connection.close()
 
-connection = create_connection()
-if connection:
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS pcaps (
-        pcap_id SERIAL PRIMARY KEY,
-        pcap_filepath TEXT NOT NULL UNIQUE,
-        txt_filepath TEXT,
-        group_id INT REFERENCES pcap_groups(group_id)
-    );
-    '''
-    execute_query(connection, create_table_query)
+# connection = create_connection()
+# if connection:
+#     create_table_query = '''
+#     CREATE TABLE IF NOT EXISTS pcaps (
+#         pcap_id SERIAL PRIMARY KEY,
+#         pcap_filepath TEXT NOT NULL UNIQUE,
+#         txt_filepath TEXT,
+#         group_id INT REFERENCES pcap_groups(group_id)
+#     );
+#     '''
+#     execute_query(connection, create_table_query)
 
-    connection.close()
+#     connection.close()
 
-connection = create_connection()
-if connection:
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS vectors (
-        doc_id SERIAL PRIMARY KEY,
-        doc_content TEXT,
-        embedding VECTOR(3072),
-        pcap_filepath TEXT REFERENCES pcaps(pcap_filepath),
-        pcap_id INT REFERENCES pcaps(pcap_id),
-        group_id INT REFERENCES pcap_groups(group_id)
-    );
-    '''
-    execute_query(connection, create_table_query)
+# connection = create_connection()
+# if connection:
+#     create_table_query = '''
+#     CREATE TABLE IF NOT EXISTS vectors (
+#         doc_id SERIAL PRIMARY KEY,
+#         doc_content TEXT,
+#         embedding VECTOR(3072),
+#         pcap_filepath TEXT REFERENCES pcaps(pcap_filepath),
+#         pcap_id INT REFERENCES pcaps(pcap_id),
+#         group_id INT REFERENCES pcap_groups(group_id)
+#     );
+#     '''
+#     execute_query(connection, create_table_query)
 
-    connection.close()
+#     connection.close()
     
-connection = create_connection()
-if connection:
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS protocols (
-        proto_id SERIAL PRIMARY KEY,
-        proto_filepath TEXT
-    );  
-    '''
-    execute_query(connection, create_table_query)
+# connection = create_connection()
+# if connection:
+#     create_table_query = '''
+#     CREATE TABLE IF NOT EXISTS protocols (
+#         proto_id SERIAL PRIMARY KEY,
+#         proto_filepath TEXT
+#     );  
+#     '''
+#     execute_query(connection, create_table_query)
 
-    connection.close()
+#     connection.close()
